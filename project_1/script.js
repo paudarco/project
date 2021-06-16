99c64f6e5e899fa245ac50e657caba7a02c26069
Lord <dasvero@gmail.com>
2021-06-16 23:51:21 +0300

    first pr is done

---
+++ project_1/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+let movie = prompt('Последний просмотренный фильм:', '');
+
+personalMovieDB.movies[movie] = prompt('На сколько вы оцение его?', '');
+
+console.log(personalMovieDB);
