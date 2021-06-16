e822586326847d3ead46345cc4694b5c2238aae3
Lord <dasvero@gmail.com>
2021-06-16 23:13:09 +0300

    first commit

---
+++ module1/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+alert('соси бибу');
+
+let result = confirm('you gay');
+
+console.log(result);
+
+let result2 = prompt('how old are you?', '11');
+
+console.log(result2);
+
+document.write('result2');
