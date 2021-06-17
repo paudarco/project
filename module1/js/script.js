edfd8c97ea2b468998c2cadc45cfa21bf340155c
Lord <dasvero@gmail.com>
2021-06-17 18:45:30 +0300

    while, do while, for, if, ternar

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,10 +1,44 @@
 'use strict';
-a = 5;
-console.log(a);
 
-a++;
-console.log(a);
+let num = 50;
 
-var a = 10;
+(num === 60) ? (
+    console.log('ok!'),
+    num = 50
+) : (num === 50) ? (
+    console.log('не оk!'),
+    num = 60
+) : (
+    console.log('nuuu hz')
+);
 
-console.log(a);
+// то же самое, что и 
+if (num === 60) {
+    console.log('ok!');
+} else {
+    console.log('не ok!');
+}
+
+num = 50;
+
+//switch проверяет на строгое равенство (num === 50)
+switch (num) {
+    case 50:
+        console.log('very ok!');
+        break;
+    case 60:
+        console.log('not seems so good');
+        break;
+    default:
+        console.log('idkhbtfm');
+}
+
+let number = 50;
+
+while (number < 55) {
+    console.log(++number);
+}
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
