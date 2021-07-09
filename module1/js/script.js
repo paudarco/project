b8f07cdcf2a2b5a1e98ad6020b04335805e3bd74
Lord <dasvero@gmail.com>
2021-07-09 21:23:11 +0300

    Objects and destructing

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,26 +1,32 @@
 'use strict';
 
-function first() {
-    //Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+let options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(Object.keys(options['colors']));
+
+const {border, bg} = options.colors;
+
+console.log(bg);
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+for (let key in options) {
+    if (typeof(options[key]) == 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]} `);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]} `);
+    }
 }
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('I\'ve done with this lesson');
-}
-
-learnJS('javascript', done);
