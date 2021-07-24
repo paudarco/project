762b3598c0216ff6eb9974d28406e0418002610e
Lord <dasvero@gmail.com>
2021-07-24 03:08:36 +0300

    navigation at DOM

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,31 +1,40 @@
 'use strict';
 
-const btn = document.querySelector('button'),
-      overlay = document.querySelector('.overlay');
+// Получение всего тега html (т.е. всей страницы)
+console.log(document.documentElement);
 
-// btn.addEventListener('click', (event) => {
-//     console.log(event.target);
-// });
+// Обращение к head
+console.log(document.head);
 
-// Всплытие событий - когда обработчик событий сначала срабатывает на дочернем элементе, 
-// потом на родителе, и выше по иерархии
-let i = 0;
-const deleteElement = (event) => {
-    console.log(event.currentTarget);
-    i++;
-    if (i == 1) {
-        event.target.removeEventListener('click', deleteElement);
+// Получение всех дочерних элементов body
+console.log(document.body.childNodes);
+
+// Получение первого и последнего дочернего элемента
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+
+// Обращение к родителю. Чтобы пойти выше, просто допишем еще .parentNode
+console.log(document.querySelector('button').parentNode);
+
+// Обращение к элементу по его CSS-селектору атрибуту
+console.log(document.querySelector('[data-current="3"'));
+
+// Получение предыдущего и следующего соседа
+console.log(document.querySelector('div').previousSibling);
+console.log(document.querySelector('div').nextSibling);
+
+// Все эти команды получают именно узлы, а не элементы. 
+// Чтобы получить именно элемент, нужно дописать Element в одном месте 
+// или заменить Node на Element. Например:
+console.log(document.body.firstElementChild);
+console.log(document.querySelector('button').parentElement);
+console.log(document.querySelector('div').previousElementSibling);
+
+// Но если мы хотим среди узлов найти элемент, можем проверить каждый узел на имя
+// Правда комментарии в этом примере не будут считаться текстовыми узлами
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
     }
-};
-
-// once - сработает только один раз
-btn.addEventListener('click', deleteElement, {once: true});
-overlay.addEventListener('click', deleteElement);
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', (event) => {
-    // preventDefault отменяет дефолтное поведение элемента
-    event.preventDefault();
-    console.log('fuck its doesnt work');
-})
+    console.log(node);
+}
