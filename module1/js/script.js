5a5d7f47c697501cb13e82c88a9f06e6f6042b74
Lord <dasvero@gmail.com>
2021-07-25 23:06:59 +0300

    defer and async

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,36 +1,6 @@
 'use strict';
 
-// touchstart срабатывает при касании по элементу
-// touchmove срабатывает при движении пальца по элементу
-// touchend срабатывает когда палец отрывается с элемента
-// touchenter срабатывает когда мы ведем пальцем по экрану и наскакиваем на нужный элемент
-// touchleave когда палец соскользнул с элемента дальше по экрану
-// touchcancel когда палец выйдет за пределы браузера
-
-// вместо window можно использовать document
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('div.box');
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-
-        console.log('start');
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log('moving...');
-    });
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-
-        console.log('ended');
-    });
-});
-
-// свойства события event для мобилок
-// touches выдает список всех пальцев на экране (event.touches)
-// targetTouches выдает список всех пальцев на элементе
-// changedTouches список пальцев участвующих в текущем событии
+const script = document.createElement('script');
+script.src = 'js/test.js';
+script.async = false;
+document.body.append(script);
