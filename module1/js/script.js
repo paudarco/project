e1228ae104049ed0025af241e2e4be20a0b23c81
Lord <dasvero@gmail.com>
2021-08-11 02:08:16 +0300

    this context

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,28 +1,77 @@
 'use strict';
 
-// Класс по факту является функцией
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello, ${this.name}`);
-    };
-}
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
 
-// С помощью свойства prototype мы можем добавлять переменные классу
-// Здесь мы добавляем функцию Exit
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`);
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+
+// obj.sum();
+
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + ' ' + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// Ручное присвоение конекста. Чтобы вызвать функцию к объекту, используем либо call, либо apply
+// При этом передавая аргументы так, как записано ниже
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+
+// function count(num) {
+//     return this * num;
+// }
+
+// Создаем контекст вручную (в скобках bind указывается аргумент this)
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+
+// 1) В обычной функции this = window, но если стоит строгий режим, то this = undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+
+const btn = document.querySelector('button');
+
+// Здесь контекстом вызова будет сам элемент, на котором произошло событтие (this работает как event.target)
+// (только если колбек в класическом виде function() {})
+btn.addEventListener('click', function() {
+    console.log(this);
+});
+
+// У стрелочной функции нет своего контекста вызова
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
 };
-// И вызвать данную переменную/функцию можно будет только после ее объявления (т. е. после 15 строки)
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
-
-ivan.hello();
-alex.hello();
-
-ivan.exit();
-
-console.log(ivan);
+obj.sayNumber();
