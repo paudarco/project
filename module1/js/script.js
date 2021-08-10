57557594a5efa919d8ea01fe04a8d133e60d00dc
Lord <dasvero@gmail.com>
2021-08-11 00:46:16 +0300

    constructors

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,52 +1,28 @@
 'use strict';
 
-const box = document.querySelector('.box');
+// Класс по факту является функцией
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello, ${this.name}`);
+    };
+}
 
-// Все значения через client/offset/scroll мы получаем в пикселях, но сами пиксели не записываются
+// С помощью свойства prototype мы можем добавлять переменные классу
+// Здесь мы добавляем функцию Exit
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+// И вызвать данную переменную/функцию можно будет только после ее объявления (т. е. после 15 строки)
 
-// ширина и высота контента блока (элемент без паддингов, бордеров и скроллеров)
-// const width = box.clientWidth,
-//       height = box.clientHeight;
-// Высота видимой части страницы без прокрутки (контент экрана)
-// const viewHeight = document.documentElement.clientHeight;
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
 
-// ширина и высота элемента с падингами, бордерами и скроллерами
-// const width = box.offsetWidth,
-//       height = box.offsetHeight;
+ivan.hello();
+alex.hello();
 
-// щирина и высота контента с учетом всех прокрутк (если стоит border-box, то берутся значения всех вложенных элементов)
-const width = box.scrollWidth,
-      height = box.scrollHeight;
+ivan.exit();
 
-document.querySelector('button').addEventListener('click', () => {
-    // box.style.width = width + 'px';
-    // box.style.height = height + 'px';
-    // scrollTop и scrollLeft показывают, сколько мы уже пролистали страницу
-    // Их мы можем изменять, в то время как scrollWidth/offsetWidth не можем
-    console.log(box.scrollTop);
-    box.scrollTop = 500;
-});
-
-// можем получить полные координаты элемента (координаты тут ВСЕГДА отсчитываются от левого верхнего угла)
-// возвращается псевдомассив из которого можно доставать элементы 
-console.log(box.getBoundingClientRect().top);
-
-
-// Если мы хотим получить значение какого-то CSS свойства, то используем getComputedStyle (применяется именнно к window)
-// Также мы можем получить стили псевдоэлементов (при этом изменить их никак не получится)
-// const style = window.getComputedStyle(box, ':after');
-const style = window.getComputedStyle(box);
-console.log(style.display);
-
-// Если мы хотим получить значение, на которое пролистана вся страица, то обращаемся через
-document.documentElement.scrollTop = 300;
-// или
-console.log(window.pageYOffset);
-
-// Если хотим пролистать страницу на какой-то уровень, то используем scrollBy/To
-// By пролистывает от настоящего положения экрана,
-// to пролистывает от начала страницы
-window.scrollBy = 400;
-window.scrollTo = 500;
-
-// console.log(width, height);
+console.log(ivan);
