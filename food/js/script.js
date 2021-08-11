234078a4d7bf8541810d25c29ec0e5883276de21
Lord <dasvero@gmail.com>
2021-08-11 19:29:56 +0300

    dinamic menu added

--- food/js/script.js
+++ food/js/script.js
@@ -137,4 +137,85 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+
+    // Food menu
+    class FoodMenuItem {
+        constructor(src, alt, title, description, price) {
+            this.src = `./img/tabs/${src}`;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+        }
+
+        load(selector) {
+            // block.querySelector('img').setAttribute('alt', this.alt);
+            // block.querySelector('img').setAttribute('src', this.src);
+            // block.querySelector('.menu__item-subtitle').textContent = this.title;
+            // block.querySelector('.menu__item-descr').textContent = this.description;
+            // block.querySelector('.menu__item-total span').textContent = this.price;
+
+            const parent = document.querySelector(selector);
+            const element = document.createElement('div');
+
+            element.innerHTML = `<div class="menu__item">
+            <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">Меню "${this.title}"</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>`;
+
+            parent.append(element);
+        }
+    }
+
+    // const food1 = new FoodMenuItem(
+    //     'Фитнес', 
+    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    //     229
+    // );
+    // const food2 = new FoodMenuItem(
+    //     'Премиум', 
+    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    //     550
+    // );
+    // const food3 = new FoodMenuItem(
+    //     'Постное',
+    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    //     430
+    // );
+
+    const foodItems = [
+        new FoodMenuItem(
+            'vegy.jpg',
+            'vegy',
+            'Фитнес', 
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            229
+        ),
+        new FoodMenuItem(
+            'elite.jpg',
+            'elite',
+            'Премиум', 
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            550
+        ),
+        new FoodMenuItem(
+            'post.jpg',
+            'post',
+            'Постное',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            430
+        )
+    ];
+
+    foodItems.forEach(item => {
+        item.load('.menu .container');
+    });
+    
 });
