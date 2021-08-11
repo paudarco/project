fd41766f7dd7bc7cfdda02c09e2a0c12a7e23b42
Lord <dasvero@gmail.com>
2021-08-11 19:32:07 +0300

    classes

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,77 +1,36 @@
 'use strict';
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
-
-//     console.log(sum());
-// }
-// showThis(4, 5);
-
-
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         console.log(this);
-//     }
-// };
-
-// obj.sum();
-
-
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + ' ' + surname);
-// }
-
-// const user = {
-//     name: 'John'
-// };
-
-// Ручное присвоение конекста. Чтобы вызвать функцию к объекту, используем либо call, либо apply
-// При этом передавая аргументы так, как записано ниже
-// sayName.call(user, 'Smith');
-// sayName.apply(user, ['Smith']);
-
-
-// function count(num) {
-//     return this * num;
-// }
-
-// Создаем контекст вручную (в скобках bind указывается аргумент this)
-// const double = count.bind(2);
-// console.log(double(3));
-// console.log(double(13));
-
-
-// 1) В обычной функции this = window, но если стоит строгий режим, то this = undefined
-// 2) Контекст у методов объекта - сам объект
-// 3) this в конструкторах и классах - это новый экземпляр объекта
-// 4) Ручная привязка this: call, apply, bind
-
-
-const btn = document.querySelector('button');
-
-// Здесь контекстом вызова будет сам элемент, на котором произошло событтие (this работает как event.target)
-// (только если колбек в класическом виде function() {})
-btn.addEventListener('click', function() {
-    console.log(this);
-});
-
-// У стрелочной функции нет своего контекста вызова
-const obj = {
-    num: 5,
-    sayNumber: function() {
-        const say = () => {
-            console.log(this.num);
-        };
-
-        say();
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
     }
-};
 
-obj.sayNumber();
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+// extends обозначает суперкласс (на основе которого создается след класс)
+// Чтобы получить все аргументы класса-родителя, мы прописываем их в super
+// При этом методы тоже переносятся
+class ColoredRectangleWithText extends Rectangle {
+    constructor(width, height, text, bgColor) {
+        super(width, height);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Цвет ${this.bgColor}, текст ${this.text}`);
+    }
+}
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(100, 20);
+const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
+
+console.log(square.calcArea());
+console.log(long.calcArea());
+
+console.log(div.calcArea());
