7202fa0249dfd3dc6334aec65f8778287923030e
Lord <dasvero@gmail.com>
2021-08-28 23:22:48 +0300

    promise

--- module1/js/script.js
+++ module1/js/script.js
@@ -1,47 +1,95 @@
 'use strict';
 
-const inputRUB = document.querySelector('#rub'),
-      inputUSD = document.querySelector('#usd');
-
-inputRUB.addEventListener('input', () => {
-    // При вводе текста создаем запрос
-    const request = new XMLHttpRequest();
-
-    // НАСТРОЙКА ЗАПРОСА
-    // open() собирает настройки, которые помогут в будущем сделать запрос
-    // Первый аргумент - метод, используемый для запроса (get или post)
-    // Второй - путь к серверу (к файлу базы данных)
-    // Третий - отвечает за ассинхронность (ajax-запросы априори ассинхронный код, но это изменяемо) (необязательный)
-    // Четвертый и пятый - логин и пароль (необязательные)
-    // request.open(method, url, async, login, pass);
-    request.open('GET', './js/current.json');
-
-    // Как именно серверу читать запрос? setRequestHeader описывает запрос
-    // Первый аргумент - обозначение, что есть тип (ключ)
-    // Второй - какой именно это тип. В нашем случае - application/json (значение)
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    
-    // Теперь отправляем запрос с помощью мтеода send()
-    request.send();
+// console.log('Запрос данных...');
 
 
-    // Свойства XMTHttpRequest: 
-    // status - статус запроса (его код) (404, 0, 200 и т.д.)
-    // statusText - статус запроса (его наименование в виде текста)
-    // response - ответ от сервера на запрос (также есть responseText)
-    // readyState - содержит текущее состояние запроса (всего 5 состояний) 
-    // https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest/readyState
+// Promise - это специальный объект, который связывает "создающий" и "потребляющий" код
+// Функция внутри промиса при создании называется исполнитель (executor)
+// Промис может находиться в трех состояниях:
+// -ожидание (pending)
+// -исполнено (fulfilled)
+// -отклонено (rejected)
 
-    // События запроса
-    // requeststatechange отслеживает статус готовности запроса в данный момент и вызывает при изменении readystate
-    // при этом условие будет   if (request.readyState === 4 && request.status === 200)
-    // load срабатывает только тогда, когда запрос полностью загрузился (при этом readystate не отслеживается)
-    request.addEventListener('load', () => {
-        if (request.status === 200) {
-            const data = JSON.parse(request.response);
-            inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
-        } else {
-            inputUSD.value = 'Что-то пошло не так';
-        }
+// Resolve нужен для кода без ошибок, reject - На случай ошибки
+// resolve вызывается через then, а reject - через catch (добавляется куда)
+// finally выведется при любом исходе
+// resolve и reject могут принимать в себя только один аргумент (или ноль)
+
+
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+
+//         resolve(product);
+//     },2000);
+// });
+
+
+// then может принимать в себя две функции: одну - для resolve, вторую - для reject
+// при этом вид будет такой: req.then(result => console.log(result), error => console.error(error))
+// если указана одна - это для resolve
+
+// catch принимает в себя только функцию с ошибкой error,
+// так что then(null => {}, error => {}) то же самое, что и catch(error => {})
+
+// finally сработает в любом случае
+// но он не имеет аргументов, и предназначен для выполнения каких-то общих действий
+// он пропускает результат или ошибку дальше, к последующим обработчикам
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+
+//             reject();
+//         }, 2000);
+//     });
+// }).then((data) => {
+//     data.modify = true;
+//     return data;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('Произошла ошибка');
+// }).finally(() => {
+//     console.log('finally');
+// });
+
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
     });
+}
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all принимает в себя массив с промисами
+// он ждет выполнения всех промисов в массиве и только потом выполняет then
+// но если хотя бы один промис отклонен, all тоже не выполнится
+// аналог - allSettled (который вернет массив с результатами всех промиссов и выполнится в любом случае)
+// так что можем заменить им 48 и 49 строки
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+// race (гонка) выполняет then когда хотя бы один промис отработал
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
+
+
+
+const req = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('1 этап');
+
+    },2000);
 });
